acb1364f2039b2c8b08fd1735c38c56acc8ec3aa
agurung01 <gurungaatish1@gmail.com>
2018-07-18 15:06:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-function dwarfRollCall(dwarves) {["Doc", "Dopey", "Bashful", "Grumpy"]
-} return {
-  "1. Doc 2. Dopey 3. Bashful 4. Grumpy"
+function dwarfRollCall(dwarves) {
 }
 
 function summonCaptainPlanet(planeteerCalls){
