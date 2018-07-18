17847decbd349433bba010155c192252acba3d78
agurung01 <gurungaatish1@gmail.com>
2018-07-18 15:25:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function dwarfRollCall(dwarves) {
-  
+  for(var i =0; i < dwarves.length; i++) {
+    return (i+1) + '. ' + dwarves[i] + ' ' + (i+2) + '. ' + dwarves[i+1] + ' ' + (i+3) + '. ' + dwarves[i+2] + ' ';
+  }
 }
 
 function summonCaptainPlanet(planeteerCalls){
